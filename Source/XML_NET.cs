b0f5c3fb2c9421103c754022765497c064c969b7
Vojtěch Kursa <it.vojtechkursa@gmail.com>
2021-12-10 19:25:16 +0100

    Add XML (.NET standard) import/export

---
+++ Source/XML_NET.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography;
+
+namespace RSAParametersIO
+{
+    public static partial class RSAParamsIO
+    {
+        //Maybe make more effective when DataEncoding.XML is available?
+
+        public static string ToXML_NET(RSA rsa, bool includePrivateParameters)
+        {
+            return rsa.ToXmlString(includePrivateParameters);
+        }
+
+        public static string ToXML_NET(RSAParameters rsa, bool includePrivateParameters)
+        {
+            RSA x = RSA.Create();
+            x.ImportParameters(rsa);
+
+            return x.ToXmlString(includePrivateParameters);
+        }
+
+        public static RSA FromXML_NET(string xml)
+        {
+            RSA rsa = RSA.Create();
+
+            rsa.FromXmlString(xml);
+
+            return rsa;
+        }
+    }
+}
